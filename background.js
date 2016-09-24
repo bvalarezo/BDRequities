bf7bf1ea32f69d1bf23646fb83c1a35efe90e136
bvalarez0 <xbxvx97@gmail.com>
2016-09-24 01:28:37 -0400

    Bunch of stuff
    read

---
+++ background.js
@@ -0,0 +1,16 @@
+var bgs = [
+    'ny_1.jpg',
+    'ny_2.jpeg'
+
+];
+var i = 0;
+
+function changeBG() {
+    document.getElementById('welcome').style.backgroundImage = "url(images/backgrounds/" + bgs[i] + ")";
+    i = (i+1) % bgs.length;
+}
+
+window.onload = function () {
+  setInterval(changeBG, 5000);
+  changeBG();
+};
